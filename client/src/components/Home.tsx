4e05afe2a18a9c32d41dc455d7457458e9b27ef0
chenhaojie9527 <a18520145493@163.com>
2023-03-28 01:01:10 +0800

    feat: Home

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,7 +1,22 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 const Home: FC = () => {
-  return <div>Home</div>;
+    const [username, setUsername] = useState("");
+  return (
+    <div className="home">
+      <h2>Sign in to your todo-list</h2>
+      <form>
+        <label htmlFor="username">Your Username</label>
+        <input
+          value={username}
+          required
+          onChange={(e) => setUsername(e.target.value)}
+          className="input"
+        />
+        <button>SIGN IN</button>
+      </form>
+    </div>
+  );
 };
 
 export default Home;
